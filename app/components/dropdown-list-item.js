c64e013e2e769a0349dd3842cfa69346f66eddd0
dougbanville <doug.banville@rte.ie>
2019-09-21 16:11:57 +0100

    state management

--- app/components/dropdown-list-item.js
+++ app/components/dropdown-list-item.js
@@ -1,5 +1,26 @@
 import Component from "@ember/component";
+import { computed } from "@ember/object";
 
 export default Component.extend({
-  tagName: "li"
+  tagName: "li",
+
+  activeItem: null,
+
+  isActive: computed("activeItem", function() {
+    if (this.activeItem === this) {
+      return true;
+    }
+  }),
+  //allow others to pass in
+  onMouseEnter() {},
+
+  onMouseLeave() {},
+
+  mouseEnter() {
+    this.onMouseEnter(this);
+  },
+
+  mouseLeave() {
+    this.onMouseLeave(this);
+  }
 });
